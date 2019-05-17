52e8e02f178662f2ae86fe009f657b05203f2801
2jungwon <james66won@gmail.com>
2019-05-17 23:56:24 +0900

    Initial Commit

---
+++ MyMovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 이정원 on 17/05/2019.
+//  Copyright © 2019 Jungwon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
