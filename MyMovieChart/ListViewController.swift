6f00c9c98d4ee6f26fde3f5d57a151687cf3efb2
2jungwon <james66won@gmail.com>
2019-06-01 12:38:34 +0900

    done

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -9,28 +9,73 @@
 import UIKit
 class ListViewController: UITableViewController {
     
-    var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
-    ]
+    var page = 1
     
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        
-        for (title, desc, opendate, rating) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            datalist.append(mvo)
-        }
         return datalist
     }()
     
+    @IBOutlet var moreBtn: UIButton!
+    @IBAction func more(_ sender: Any) {
+        self.page+=1
+        self.callMovieAPI()
+        self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
+        self.callMovieAPI()
+    }
+    
+    func callMovieAPI() {
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        let apidata = try! Data(contentsOf: apiURI)
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다"
+        NSLog("API Result=\(log)")
         
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            for row in movie {
+                let r = row as! NSDictionary
+                let mvo = MovieVO()
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                let url: URL! = URL(string: mvo.thumbnail!)
+                let imageData = try! Data(contentsOf: url)
+                mvo.thumbnailImage = UIImage(data:imageData)
+                
+                self.list.append(mvo)
+            }
+            
+            let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+            if (self.list.count >= totalCount) {
+                self.moreBtn.isHidden = true
+            }
+        } catch {
+            NSLog("Parse Error!!")
+        }
+    }
+    
+    func getThumbnailImage(_ index: Int) -> UIImage {
+        let mvo = self.list[index]
+        if let savedImage = mvo.thumbnailImage {
+            return savedImage
+        } else {
+            let url: URL! = URL(string: mvo.thumbnail!)
+            let imageData = try! Data(contentsOf: url)
+            mvo.thumbnailImage = UIImage(data:imageData)
+            return mvo.thumbnailImage!
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,18 +84,21 @@ class ListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = self.list[indexPath.row]
+        NSLog("호출된 행번호 : \(indexPath.row), 제목:\(row.title!)")
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
-        let title = cell.viewWithTag(101) as? UILabel
-        let desc = cell.viewWithTag(102) as? UILabel
-        let opendate = cell.viewWithTag(103) as? UILabel
-        let rating = cell.viewWithTag(104) as? UILabel
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
         
-        title?.text = row.title
-        desc?.text = row.description
-        opendate?.text = row.opendate
-        rating?.text = "\(row.rating!)"
+        DispatchQueue.main.async(execute: {
+            NSLog("비동기 방식으로 실행되는 부분입니다.")
+            cell.thumbnail.image = self.getThumbnailImage(indexPath.row)
+        })
+        
+        NSLog("메소드 실행을 종료하고 셀을 리턴합니다.")
         
         return cell
     }
---
+++ MyMovieChart/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 이정원 on 18/05/2019.
+//  Copyright © 2019 Jungwon Lee. All rights reserved.
+//
+
+import UIKit
+class MovieCell: UITableViewCell {
+    
+  
+    @IBOutlet var title: UILabel!
+    @IBOutlet var opendate: UILabel!
+    @IBOutlet var rating: UILabel!
+    @IBOutlet var desc: UILabel!
+    @IBOutlet var thumbnail: UIImageView!
+}
